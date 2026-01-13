c9bf8dd686b349e32e3e1ab91dc05579cedb35be
Gamshikk <glebovich157rtyp@gmail.com>
2026-01-13 04:08:48 +0300

    fix(chat): make loading messages when chat created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,6 +40,7 @@ export default function Page() {
           queueMicrotask(() => {
             setActiveThreadId(serverThreadId);
             fetchThreads();
+            loadThreadMessages(serverThreadId);
           });
         }
 
